79f0fa126d166bfad6439914747d2cbb9d2bdff2
Dhanush-S26 <dhanush260202@gmail.com>
2025-08-24 18:41:23 +0530

    First Commit

---
+++ project1.js
@@ -0,0 +1,9 @@
+// Responsive Header Section
+var responsenavbar = document.getElementById("responsenavbar")
+
+function openSideNavBar(){
+    responsenavbar.style.display="block";
+}
+function closeSideNavBar(){
+    responsenavbar.style.display="none";
+}
